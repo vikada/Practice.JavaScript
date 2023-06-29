ba3a3d8731faec5a2f6fa801bb333fc654db360d
Viktoria Danilenko <viky.pan4enko@gmail.com>
2023-06-29 20:11:33 +0300

    first commit

---
+++ 1_task.js
@@ -0,0 +1,84 @@
+    // проверить, четное число или нет
+    // function evenOdd(num) {
+    //     if (num % 2 === 0) {
+    //         console.log('even');
+    //     } else {
+    //         console.log('odd');
+    //             }
+    //         }
+    // evenOdd(5);
+    // evenOdd(8);
+    // evenOdd(164);
+
+    // проверить, что первые буквы двух слов совпадают
+    // function check(str1, str2) {
+    //     if (str1[0] === str2[0]) {
+    //         console.log('same');
+    //     } else {
+    //         console.log('different');
+    //     }
+    // }
+    // check('string', 'name');
+    // check('sun', 'son');
+
+    // получить последнюю букву слова. Если слово заканчивается на мягкий знак, то получить предпоследнюю букву
+    // function get(str) {
+    //     if (str[str.length-1] === 'ь') {
+    //         console.log(str[str.length-2]);
+    //     } else {
+    //         console.log(str[str.length-1]);
+    //     }
+    // }
+    // get('мышь');
+    // get('кошка');
+    
+    // получить последнюю букву слова
+    // let string = 'string';
+    // console.log(string[string.length-1])
+
+    // вывести первую цифру числа
+    // вывести последнюю цифру числа
+    // вывести сумму первой и последней цифры
+    // вывести количество цифр числа
+    // function get(num) {
+    //     console.log(String(num)[0]);
+    //     let newNum = String(num);
+    //     console.log(newNum[newNum.length-1]);
+    //     console.log(Number(newNum[0]) + Number(newNum[newNum.length-1]))
+    //     console.log(newNum.length);
+    // }
+    // get(125);
+
+    // проверить, что первые цифры двух чисел совпадают
+    // function checkNum(num1, num2) {
+    //     if (String(num1)[0] === String(num2)[0]) {
+    //         console.log('same');
+    //     } else {
+    //         console.log('different');
+    //     }
+    // }
+    // checkNum(24, 56);
+    // checkNum(42, 42);
+
+    // если в строке более одного символа, вывести предпоследний символ этой строки
+    // function get(str) {
+    //     if (str.length > 1) {
+    //         console.log(str[str.length-2]);
+    //     } else {
+    //         console.log('just one letter');
+    //     }
+    // }
+    // get('string');
+    // get('f');
+
+    // даны два целых числа. Проверить, что первое число без остатка делится на второе
+    // function get(num1, num2) {
+    //     if ((num1 / num2) % 2 === 0) {
+    //         console.log('is divisible');
+    //     } else {
+    //         console.log('no is divisible');
+    //     }
+    // }
+    // get(10, 5);
+    // get(25, 3);
+
